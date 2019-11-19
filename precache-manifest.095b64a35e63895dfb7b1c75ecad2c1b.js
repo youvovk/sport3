7d3cc2e8ea186bf5730ccce12df2d2ecb5f9c171
youvovk <yuriy3vovk@gmail.com>
2019-11-19 18:11:55 +0200

    Updates

---
+++ precache-manifest.095b64a35e63895dfb7b1c75ecad2c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c603e83920570859006084a8d82c70c",
+    "url": "/sport3/index.html"
+  },
+  {
+    "revision": "aa85e4b5df41e783d28d",
+    "url": "/sport3/static/css/main.7989ee8c.chunk.css"
+  },
+  {
+    "revision": "65edacc92c98195c9833",
+    "url": "/sport3/static/js/2.6ee85c7e.chunk.js"
+  },
+  {
+    "revision": "aa85e4b5df41e783d28d",
+    "url": "/sport3/static/js/main.53c42773.chunk.js"
+  },
+  {
+    "revision": "b2c63609c4ceccf0616d",
+    "url": "/sport3/static/js/runtime-main.d6ba339c.js"
+  },
+  {
+    "revision": "7970ffd2a9f4a27996fd920e79e1d7e3",
+    "url": "/sport3/static/media/back.7970ffd2.jpg"
+  }
+]);
